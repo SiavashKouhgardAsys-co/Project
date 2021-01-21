73d0207f2279938d42c1dea47560ad642ec0445a
siavash kouhgard <siavashkouhgard75@gmail.com>
2021-01-21 22:27:28 +0330

    Update Statrt Up

--- MoshaverAmlak/Startup.cs
+++ MoshaverAmlak/Startup.cs
@@ -25,7 +25,7 @@ namespace MoshaverAmlak
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
