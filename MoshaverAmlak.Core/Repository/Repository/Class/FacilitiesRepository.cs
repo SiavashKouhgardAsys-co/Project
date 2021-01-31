834f9c52666ef97319bdd3e35cfb84d06ac510e7
siavash kouhgard <siavashkouhgard75@gmail.com>
2021-01-31 15:56:47 +0330

    add facilityRepository

---
+++ MoshaverAmlak.Core/Repository/Repository/Class/FacilitiesRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using MoshaverAmlak.Core.Repository.Repository.Interface;
+using MoshaverAmlak.DataLayer.Common;
+using MoshaverAmlak.DataLayer.Entity;
+using MoshaverAmlak.DataLayer.GenericRepository;
+
+namespace MoshaverAmlak.Core.Repository.Repository.Class
+{
+    public class FacilitiesRepository : IFacilitiesRepository
+    {
+        private readonly IGenericRepository<Facilities> _facilitiesRepository;
+
+        public FacilitiesRepository(IGenericRepository<Facilities> facilitiesRepository)
+        {
+            _facilitiesRepository = facilitiesRepository;
+        }
+
+        public ReturnEntity_IQueryable<Facilities> GetAllEntity() => _facilitiesRepository.GetAllEntity();
+        public ReturnEntity<Facilities> GetFacilityById(int facilityId) => _facilitiesRepository.GetEntityById(facilityId);
+        public async Task<Result> CreateFacility(Facilities facilities)
+        {
+            var entity = _facilitiesRepository.AddEntity(facilities);
+            if (entity.StatusResult != (int)Result.Status.OK) return entity;
+            return await _facilitiesRepository.SaveChangeAsync();
+        }
+        public async Task<Result> DeleteFacilityById(int facilityId)
+        {
+            var entity = _facilitiesRepository.DeleteEntityById(facilityId);
+            if (entity.StatusResult != (int)Result.Status.OK) return entity;
+            return await _facilitiesRepository.SaveChangeAsync();
+        }
+        public async Task<Result> UpdateFacility(Facilities facilities)
+        {
+            var entity = GetFacilityById(facilities.Id);
+            if (entity.Result.StatusResult != (int)Result.Status.OK) return entity.Result;
+            entity.Entity.Name = facilities.Name;
+            var update_entity = _facilitiesRepository.UpdateEntity(entity.Entity);
+            if (update_entity.StatusResult != (int)Result.Status.OK) return update_entity;
+            return await _facilitiesRepository.SaveChangeAsync();
+        }
+
+        public void Dispose()
+        {
+            _facilitiesRepository?.Dispose();
+        }
+    }
+}
---
+++ MoshaverAmlak.Core/Repository/Repository/Interface/IFacilitiesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using MoshaverAmlak.DataLayer.Common;
+using MoshaverAmlak.DataLayer.Entity;
+
+namespace MoshaverAmlak.Core.Repository.Repository.Interface
+{
+    public interface IFacilitiesRepository : IDisposable
+    {
+        ReturnEntity_IQueryable<Facilities> GetAllEntity();
+        ReturnEntity<Facilities> GetFacilityById(int facilityId);
+        Task<Result> CreateFacility(Facilities facilities);
+        Task<Result> DeleteFacilityById(int facilityId);
+        Task<Result> UpdateFacility(Facilities facilities);
+    }
+}
